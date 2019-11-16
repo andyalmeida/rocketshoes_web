d8890b3fc9d738e6d2fdb8e68282776b9e71cd9d
André Almeida <andalvalmeida@gmail.com>
2019-11-16 11:42:58 -0300

    Cart layout

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,97 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return <div />;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th>&nbsp;</th>
+            <th>PRODUCT</th>
+            <th>QTY</th>
+            <th>SUBTOTAL</th>
+            <th>&nbsp;</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-olympikus-random/06/D22-1962-006/D22-1962-006_detalhe2.jpg?ims=326x"
+                alt="aa"
+              />
+            </td>
+            <td>
+              <strong>Product name</strong>
+              <span>$56.89</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="text" readOnly value="2" />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>$100.00</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-olympikus-random/06/D22-1962-006/D22-1962-006_detalhe2.jpg?ims=326x"
+                alt="aa"
+              />
+            </td>
+            <td>
+              <strong>Product name</strong>
+              <span>$56.89</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="text" readOnly value="2" />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>$100.00</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Checkout</button>
+        <Total>
+          <span>TOTAL</span>
+          <strong>$353.38</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
