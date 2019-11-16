5ca6f2b8e95ef1630f7d909a034cb689e1a8a28a
André Almeida <andalvalmeida@gmail.com>
2019-11-16 11:03:49 -0300

    Home layout

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,85 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { MdAddShoppingCart } from 'react-icons/md';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-random/06/D22-1962-006/D22-1962-006_detalhe2.jpg?ims=326x"
+          alt="aa"
+        />
+        <strong>Tênis legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-random/06/D22-1962-006/D22-1962-006_detalhe2.jpg?ims=326x"
+          alt="aa"
+        />
+        <strong>Tênis legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-random/06/D22-1962-006/D22-1962-006_detalhe2.jpg?ims=326x"
+          alt="aa"
+        />
+        <strong>Tênis legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-random/06/D22-1962-006/D22-1962-006_detalhe2.jpg?ims=326x"
+          alt="aa"
+        />
+        <strong>Tênis legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-random/06/D22-1962-006/D22-1962-006_detalhe2.jpg?ims=326x"
+          alt="aa"
+        />
+        <strong>Tênis legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
